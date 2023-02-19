da67629f18270fb385270d1f38ba8f7f410954a6
Mark Domahidi <mdomahidi@gmail.com>
2023-02-19 13:07:49 +0100

    fix: jest module name mapping paths

--- jest.config.cjs
+++ jest.config.cjs
@@ -10,9 +10,9 @@ module.exports = createJestConfig({
   testEnvironment: "jest-environment-jsdom",
   moduleDirectories: ["node_modules"],
   moduleNameMapper: {
-    "^@components(.*)$": "<rootDir>/components$1",
-    "^@hooks(.*)$": "<rootDir>/hooks$1",
-    "^@styles(.*)$": "<rootDir>/styles$1",
-    "^@utils(.*)$": "<rootDir>/utils$1",
+    "^@components(.*)$": "<rootDir>/src/components$1",
+    "^@hooks(.*)$": "<rootDir>/src/hooks$1",
+    "^@styles(.*)$": "<rootDir>/src/styles$1",
+    "^@utils(.*)$": "<rootDir>/src/utils$1",
   },
 });
